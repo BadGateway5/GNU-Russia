dd9646cb133b6d4ec1af47d2d9dfb151c61fcf26
BadGateway5 <khabibtime@rambler.ru>
2025-02-08 23:10:15 +0300

    update

---
+++ app/db.py
@@ -0,0 +1,22 @@
+class User:
+    
+    def __init__(self):
+        pass
+
+
+    def create_user():
+        pass
+
+
+    def get_user():
+        pass
+
+
+class Admin(User):
+    
+    def __init__(self):
+        super().__init__()
+
+    
+    def delete_user():
+        pass
